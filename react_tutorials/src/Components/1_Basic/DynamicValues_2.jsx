7d5d4edc9d89e90733962f49cdac5826232aaf44
Nipun Shrivats <nipunrawat16@gmail.com>
2025-03-26 16:14:08 +0530

    updates on 26-3-25, change no. 3

--- react_tutorials/src/Components/1_Basic/DynamicValues_2.jsx
+++ react_tutorials/src/Components/1_Basic/DynamicValues_2.jsx
@@ -2,10 +2,25 @@
 import React from "react";
 
 export default function DynamicValues_2() {
+  const name = "Queen of tears";
+  const rating = 8.2;
+  const summary = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum alias voluptates est natus nobis? Ipsam sequi ipsum corporis sed natus, optio repellat ullam odio molestiae sapiente totam dicta doloribus dolores?`;
+
+  const retirngenre = () => {
+    const genre = "Romcom";
+    return genre;
+  };
   return (
-    <div>
+    <>
       {/* 1. Using Variables :- 
     can embed any js  variable in JSX by enclosing curly braces.*/}
-    </div>
+      <div>
+        <img src="" alt="" />
+        <h2>Name: {name}</h2>
+        <h2>Rating: {rating}</h2>
+        <h2>Summary: {summary}</h2>
+        <h2>Genre: {retirngenre()}</h2>
+      </div>
+    </>
   );
 }
