4242b98424cd11d9663459d6db33b0ecfafa17c1
Nipun Shrivats <nipunrawat16@gmail.com>
2025-04-07 16:27:21 +0530

    updates on 7-3-25, change no.3

--- react_tutorials/src/Components/8_MiniProjecft_TodoApp/Time.jsx
+++ react_tutorials/src/Components/8_MiniProjecft_TodoApp/Time.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Todo.css";
 
 export default function Time() {
   const [dateTime, setDateTime] = useState("");
 
-  setInterval(() => {
-    const now = new Date();
-    const date = now.toLocaleDateString();
-    const time = now.toLocaleTimeString();
-    setDateTime(`${date} - ${time}`);
-  }, 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const date = now.toLocaleDateString();
+      const time = now.toLocaleTimeString();
+      setDateTime(`${date} - ${time}`);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  // const interval = setInterval(() => {
+  //   const now = new Date();
+  //   const date = now.toLocaleDateString();
+  //   const time = now.toLocaleTimeString();
+  //   setDateTime(`${date} - ${time}`);
+  // }, 1000);
 
   return (
     <>
