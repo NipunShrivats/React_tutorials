e9e9d5cf786dfd6e73e2aee762406471698b0871
Nipun Shrivats <nipunrawat16@gmail.com>
2025-04-07 11:43:56 +0530

    updates on 7-3-25, change no. 1

--- react_tutorials/src/Components/8_MiniProjecft_TodoApp/Todo.jsx
+++ react_tutorials/src/Components/8_MiniProjecft_TodoApp/Todo.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./todo.css";
 
+import { FaCheck } from "react-icons/fa6";
+import { MdDelete } from "react-icons/md";
+
 export default function Todo() {
+  const [inputValue, setInputValue] = useState("");
+  const [listData, setListData] = useState([]);
+
+  console.log(listData);
+
+  const handleInputChnage = (value) => {
+    setInputValue(value);
+  };
+  const handleFormSubmit = (event) => {
+    // 1. Avoid refresh
+    event.preventDefault();
+    // 2. In case empty return empty
+    if (!inputValue) return;
+    // 3. If data is repeated return nothing
+    if (listData.includes(inputValue)) return;
+    // 4. keep previous value intack while adding new value i.e inputvalue.
+    setListData((prev) => [...prev, inputValue]);
+    // 5. After value is added empty the input bar
+    setInputValue("");
+  };
   return (
     <>
       <section className="todo-app">
@@ -9,9 +32,15 @@ export default function Todo() {
           <h1>Todo List</h1>
         </header>
         <section className="form">
-          <form action="">
+          <form action="" onSubmit={handleFormSubmit}>
             <div>
-              <input type="text" className="todo-input" autoComplete="off" />
+              <input
+                type="text"
+                className="todo-input"
+                autoComplete="off"
+                value={inputValue}
+                onChange={(event) => handleInputChnage(event.target.value)}
+              />
             </div>
             <div>
               <button type="submit" className="todo-btn">
@@ -19,6 +48,27 @@ export default function Todo() {
               </button>
             </div>
           </form>
+        </section>
+
+        <section
+          className="myList"
+          style={{ padding: listData.length == 0 ? "0rem" : "2rem 4rem" }}
+        >
+          <ul>
+            {listData.map((data, index) => {
+              return (
+                <li key={index} className="todo-item">
+                  <button className="check-btn">
+                    <FaCheck />
+                  </button>
+                  <span>{data}</span>
+                  <button className="del-btn">
+                    <MdDelete />
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
         </section>
       </section>
     </>
